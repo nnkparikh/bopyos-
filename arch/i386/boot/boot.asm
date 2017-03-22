b8b9b7fdf42fa66c63a33d0fb15c482ba76b7376
Neel Parikh <nnkparikh@gmail.com>
2017-03-22 04:21:28 -0600

    multiboot header declared

---
+++ arch/i386/boot/boot.asm
@@ -0,0 +1,16 @@
+; Constants for the multiboot header
+MAGIC    equ  0x1BADB002			; magic number identifying the multiboot header
+MBALIGN  equ  1<<0              	; align loaded modules on page boundaries
+MEMINFO  equ  1<<1             		; provide memory map
+MBFLAGS  equ  MEMINFO | MBALIGN 	; multiboot flag field
+CHECKSUM equ  -(MAGIC + MBFLAGS)	; checksum for multiboot
+
+; Declare the multiboot header.
+; This header indicates to the bootloader that the kernel is multiboot compliant.
+section .multiboot 
+align 4
+dd MAGIC
+dd MBFLAGS
+dd CHECKSUM
+
+
