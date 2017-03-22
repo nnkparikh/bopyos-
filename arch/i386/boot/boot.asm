84667b99bc9195385ef2b542f440636e043463f0
Neel Parikh <nnkparikh@gmail.com>
2017-03-22 04:37:43 -0600

    [cleanup] fixed up a tab inconsistency

--- arch/i386/boot/boot.asm
+++ arch/i386/boot/boot.asm
@@ -1,5 +1,5 @@
 ; Constants for the multiboot header
-MAGIC    equ  0x1BADB002			; magic number identifying the multiboot header
+MAGIC    equ  0x1BADB002		; magic number identifying the multiboot header
 MBALIGN  equ  1<<0              	; align loaded modules on page boundaries
 MEMINFO  equ  1<<1             		; provide memory map
 MBFLAGS  equ  MEMINFO | MBALIGN 	; multiboot flag field
